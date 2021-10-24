5e858fc8489971d93c23e01f10716b4b56a84d08
Braham <webmaster@crowingram.com>
2021-10-24 18:39:04 -0400

    Adds connection to yarns.json and pulls name and mass
    Successfully pulls yarn name and manufacturer information from yarns.json
    Removes JSON template to yarns_json_format.js with comments
    Checks for imperial and metric weights for the first put-up listed in the array

---
+++ README.md
@@ -0,0 +1,3 @@
+# yarndata-fetch-sandbox
+
+This is a sandbox I'm using to develop the API for the yarn data JSON file that will store data for a new website.
---
+++ yarns_json_format.js
@@ -0,0 +1,34 @@
+[{
+	"yarn_id"             : "vendor-brand-name",
+	"vendor_name"         : "",
+	"vendor_reference"    : "",
+	"manufacturer_brand"  : "",
+	"name"                : "",
+	"published_content"   : "",
+	"content_data"        : [
+		{
+		"percentage"  : ,
+		"fiber_name"  : "",
+		"fiber_data"  : ["", "", ""]
+		}, {
+		"percentage"  : ,
+		"fiber_name"  : "",
+		"fiber_data"  : ["", "", ""]
+		}
+	],
+	"mass"                : [[null, null]],       // oz, g
+	"length"              : [[null, null]],       // yds, m
+	"knit_st_gauge"       : [[null, null]],       // sts/4˝, sts/10mm
+	"knit_row_gauge"      : [[null, null, null]], // rows/4˝, rows/10mm
+	"croc_st_gauge"       : [[null, null, null]], // sts/4˝, sts/10mm
+	"croc_row_gauge"      : [[null, null, null]], // rows/4˝, rows/10mm
+	"wraps_per_inch"      : [null],               // wraps per inch
+	"knit_needle_rec"     : [[null, null]],       // US, mm
+	"croc_hook_rec"       : [[null, null]],       // US, mm
+	"image_file_location" : "",
+	"shades"               : [
+		["", 	"",			"d.jpg", 	[null]],	// Each shade - [shade identifier, shade name, filename, [tags]]
+		["", 	"",			"d.jpg", 	[null]],
+		["", 	"",			"d.jpg", 	[null]]
+	]
+}]
